85253d05501cee56267843c45b16a073ae20c8b4
Triptixx <54292597+triptixx@users.noreply.github.com>
2019-09-12 19:08:10 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 set -e
 
-echo "king of ${INPUT_REPO}"
+echo "repo $INPUT_REPO"
+echo "path $INPUT_PATH"
+echo "dockerfile $INPUT_DOCKERFILE"
+echo "use_cache $INPUT_USE_CACHE"
+echo "no_labels $INPUT_NO_LABELS"
+echo "build_args $INPUT_BUILD_ARGS"
+echo "arguments $INPUT_ARGUMENTS"
+echo "make $INPUT_MAKE"
+echo "rm $INPUT_RM"
+echo "squash $INPUT_SQUASH"
