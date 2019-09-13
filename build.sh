692c3dcfa811ceeb481e47a3e3de4234a12f68a7
Triptixx <54292597+triptixx@users.noreply.github.com>
2019-09-13 16:02:01 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,82 @@
 #!/bin/sh
 set -e
 
-echo "repo $INPUT_REPO"
-echo "path $INPUT_PATH"
-echo "dockerfile $INPUT_DOCKERFILE"
-echo "use_cache $INPUT_USE_CACHE"
-echo "no_labels $INPUT_NO_LABELS"
-echo "build_args $INPUT_BUILD_ARGS"
-echo "arguments $INPUT_ARGUMENTS"
-echo "make $INPUT_MAKE"
-echo "rm $INPUT_RM"
-echo "squash $INPUT_SQUASH"
-echo "Test $TEST"
+# ANSI colour escape sequences
+RED='\033[0;31m'
+RESET='\033[0m'
+error() { >&2 echo -e "${RED}Error: $@${RESET}"; exit 1; }
+
+# $INPUT_REPO          tag to this repo/repo to push to
+# $INPUT_PATH          override working directory
+# $INPUT_DOCKERFILE    override Dockerfile location
+# $INPUT_BUILD_ARGS    comma/space separated build arguments
+# $INPUT_USE_CACHE     override to disable --no-cache
+# $INPUT_NO_LABELS     disable automatic image labelling
+# $INPUT_ARGUMENTS     optional extra arguments to pass to `docker build`
+# $INPUT_RM            a flag to immediately `docker rm` the built image
+# $INPUT_SQUASH        builds with --squash
+# $INPUT_MAKE          provides makeflags concurrent of nproc
+
+if [ -z "$INPUT_REPO" ]; then
+  if [ "$INPUT_RM" = true ]; then
+    INPUT_REPO="$GITHUB_REPOSITORY"
+  else
+    error "Missing 'repo' argument required for building"
+  fi
+fi
+
+# Always specify pull so images are pulled, and intermediate containers removed
+ARGS="--pull\0--force-rm"
+
+# Override Dockerfile if specified
+[ -n "$INPUT_DOCKERFILE" ] && ARGS="$ARGS\0--file=$INPUT_DOCKERFILE"
+
+# Squash image if requested
+[ "$INPUT_SQUASH" = true ] && ARGS="$ARGS\0--squash"
+
+# Specify MAKEFLAGS job concurrency flag
+[ "$INPUT_MAKE" = true ] && ARGS="$ARGS\0--build-arg\0MAKEFLAGS=-j$(nproc)"
+
+# Specify --no-cache unless caching is requested
+[ "$INPUT_USE_CACHE" = false ] && ARGS="$ARGS\0--no-cache"
+
+while read -r arg; do
+  # If arg is '%file: <filename>' then .parse and read file
+  if echo "$arg" | grep -q "%file\\s*:\\s*"; then
+    arg="${arg%%=*}=$(cat "$(echo ${arg#*:} | xargs)")"
+  fi
+  if [ -n "${arg// }" ]; then
+    # Only add arguments if they're not empty
+    # this prevents the '"docker build" requires exactly 1 argument.' error
+    ARGS="$ARGS\0--build-arg\0${arg}"
+  fi
+done << EOA
+$(echo "$INPUT_BUILD_ARGS" | tr ',' '\n')
+EOA
+
+export BUILD_DATE="$(date +"%Y-%m-%dT%H:%M:%SZ")"
+export VCS_REF="$GITHUB_SHA"
+export VCS_URL="https://github.com/$GITHUB_REPOSITORY"
+export VENDOR="$GITHUB_ACTOR"
+
+ARGS="$ARGS\0--build-arg\0BUILD_DATE=$BUILD_DATE"
+ARGS="$ARGS\0--build-arg\0VCS_REF=$VCS_REF"
+ARGS="$ARGS\0--build-arg\0VCS_URL=$VCS_URL"
+ARGS="$ARGS\0--build-arg\0VENDOR=$VENDOR"
+
+if [ "$INPUT_NO_LABELS" = false ]; then
+  ARGS="$ARGS\0--label\0org.label-schema.build-date=$BUILD_DATE"
+  ARGS="$ARGS\0--label\0org.label-schema.vcs-ref=${VCS_REF:0:7}"
+  ARGS="$ARGS\0--label\0org.label-schema.vcs-url=$VCS_URL"
+  ARGS="$ARGS\0--label\0org.label-schema.vendor=$VENDOR"
+  ARGS="$ARGS\0--label\0org.label-schema.schema-version=1.0"
+fi
+
+>&2 echo "+ docker build ${ARGS//\\0/ } $INPUT_ARGUMENTS --tag=$INPUT_REPO ${INPUT_PATH:-.}"
+
+# Un-escape the NULL characters to fix arguments with spaces in
+printf "$ARGS${INPUT_ARGUMENTS//,/\0}\0--tag=${INPUT_REPO}\0${INPUT_PATH:-.}" | xargs -0 docker build
+
+if [ "$INPUT_RM" = true ]; then
+  docker image rm "$INPUT_REPO"
+fi
